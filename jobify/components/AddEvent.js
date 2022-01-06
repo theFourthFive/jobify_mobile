308bc9b75fb98f13f22cd42649019a008c8c345b
AhmedAzizGHATTAS <azizghattas552@gmail.com>
2022-01-06 17:56:34 +0100

    createAddEventParttwo

--- jobify/components/AddEvent.js
+++ jobify/components/AddEvent.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import DatePicker from 'react-native-datepicker';
+import DatePicker from "react-native-datepicker";
 import {
   StyleSheet,
   Text,
@@ -9,74 +9,98 @@ import {
   Button,
   Platform,
   TextInput,
- 
 } from "react-native";
 export default function AddEvent() {
-    const [companyId, setCompanyId] = useState("");
-    const [eventName, setEventName] = useState("");
-    const [location, setLocation] = useState("");
-    const [date_time, setDate_time] = useState("");
-    const [imageUri, setImageUri] = useState("");
-    const [duration, setDuration] = useState("");
-    const [dailyPay, setDailyPay] = useState("");
-    const [nbrWaiter, setNbrWaiter] = useState("");
-    const [nbrChef, setNbrChef] = useState("");
-    const [nbrCleaningWorker, setNbrCleaningWorker] = useState("");
-    /* handelChange functions here*/
-    const onChangeeventNameHandler = (eventName) => {
-        setEventName(eventName);
-      };
-      const onChangelocationHandler = (location) => {
-        setLocation(location);
-      };
-      const onChangedate_timeHandler = (date_time) => {
-        setDate_time(date_time);
-      };
-      const onChangeimageUriHandler = (imageUri) => {
-        setImageUri(imageUri);
-      };
-      const onChangedurationHandler = (duration) => {
-        setDuration(duration);
-      };
-      const onChangedailyPayHandler = (dailyPay) => {
-        setDailyPay(dailyPay);
-      };
-      const onChangenbrWaiterHandler = (nbrWaiter) => {
-        setNbrWaiter(nbrWaiter);
-      };
-      const onChangenbrChefHandler = (nbrChef) => {
-        setNbrChef(nbrChef);
-      };
-      const onChangenbrCleaningWorkerHandler = (nbrCleaningWorker) => {
-        setNbrCleaningWorker(nbrCleaningWorker);
-      };
-      axios.post('/addEvent', {
-        companyId,
-        eventName,
-        location,
-        date_time,
-        imageUri,
-        duration,
-        dailyPay,
-        nbrWaiter,
-        nbrChef,
-        nbrCleaningWorker,
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    return(
-        <ScrollView contentContainerStyle={styles.container}>
+  const [companyId, setCompanyId] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [location, setLocation] = useState("");
+  const [date_time, setDate_time] = useState("");
+  const [imageUri, setImageUri] = useState("");
+  const [duration, setDuration] = useState("");
+  const [dailyPay, setDailyPay] = useState("");
+  const [nbrWaiter, setNbrWaiter] = useState("");
+  const [nbrChef, setNbrChef] = useState("");
+  const [nbrCleaningWorker, setNbrCleaningWorker] = useState("");
+  /* handelChange functions here*/
+  const onChangeeventNameHandler = (eventName) => {
+    setEventName(eventName);
+  };
+  const onChangelocationHandler = (location) => {
+    setLocation(location);
+  };
+  const onChangedate_timeHandler = (date_time) => {
+    setDate_time(date_time);
+  };
+  const onChangeimageUriHandler = (imageUri) => {
+    setImageUri(imageUri);
+  };
+  const onChangedurationHandler = (duration) => {
+    setDuration(duration);
+  };
+  const onChangedailyPayHandler = (dailyPay) => {
+    setDailyPay(dailyPay);
+  };
+  const onChangenbrWaiterHandler = (nbrWaiter) => {
+    setNbrWaiter(nbrWaiter);
+  };
+  const onChangenbrChefHandler = (nbrChef) => {
+    setNbrChef(nbrChef);
+  };
+  const onChangenbrCleaningWorkerHandler = (nbrCleaningWorker) => {
+    setNbrCleaningWorker(nbrCleaningWorker);
+  };
+//   const onSubmitFormHandler = async (event) => {
+//     try {
+//         const response = await axios.post("/addEvent", {
+//             companyId,
+//             eventName,
+//             location,
+//             date_time,
+//             imageUri,
+//             duration,
+//             dailyPay,
+//             nbrWaiter,
+//             nbrChef,
+//             nbrCleaningWorker,
+//           })}
+//   .then(function (response) {
+//           console.log(response);
+//         })
+//         .catch(function (error) {
+//           console.log(error);
+//         });
+
+//   }
+const onSubmitFormHandler = async (event) => {
+  axios
+    .post("/addEvent", {
+      companyId,
+      eventName,
+      location,
+      date_time,
+      imageUri,
+      duration,
+      dailyPay,
+      nbrWaiter,
+      nbrChef,
+      nbrCleaningWorker,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+        console.log("===>");
+      console.log(error);
+    });
+}
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
       <View>
         <View style={styles.wrapper}>
           {/* {isLoading ? (
             <Text style={styles.formHeading}> Creating resource </Text>
           ) : ( */}
-            <Text style={styles.formHeading}>Create new user</Text>
-          
+          <Text style={styles.formHeading}>Create new user</Text>
         </View>
         <View style={styles.wrapper}>
           <TextInput
@@ -93,40 +117,82 @@ export default function AddEvent() {
             placeholder="Location"
             placeholderTextColor="#ffffff"
             style={styles.input}
-            value={ location}
+            value={location}
             // editable={!isLoading}
             onChangeText={onChangelocationHandler}
           />
         </View>
-       
+
         <View style={styles.wrapper}>
-        <DatePicker
-         placeholder="Date Of The Event"
-         placeholderTextColor="#ffffff"
-         mode="datetime"
-         style={styles.input}
-         value={date_time}
-         onChangeText={onChangedate_timeHandler}
-       />
+          <DatePicker
+            placeholder="Date Of The Event"
+            placeholderTextColor="#ffffff"
+            mode="datetime"
+            style={styles.input}
+            value={date_time}
+            onChangeText={onChangedate_timeHandler}
+          />
         </View>
-        {/* <View style={styles.wrapper}>
+        <View style={styles.wrapper}>
           <TextInput
-            placeholder="Email"
+            placeholder=" Duration"
             placeholderTextColor="#ffffff"
             style={styles.input}
-            value={email}
-            editable={!isLoading}
-            onChangeText={onChangeEmailHandler}
+            value={duration}
+            // editable={!isLoading}
+            onChangeText={onChangedurationHandler}
           />
-        </View> */}
-        {/* <View>
+        </View>
+        <View style={styles.wrapper}>
+          <TextInput
+            placeholder="DailyPay"
+            placeholderTextColor="#ffffff"
+            style={styles.input}
+            value={dailyPay}
+            // editable={!isLoading}
+            onChangeText={onChangedailyPayHandler}
+          />
+        </View>
+
+        <View style={styles.wrapper}>
+          <TextInput
+            placeholder="nbrWaiter"
+            placeholderTextColor="#ffffff"
+            style={styles.input}
+            value={nbrWaiter}
+            // editable={!isLoading}
+            onChangeText={onChangenbrWaiterHandler}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <TextInput
+            placeholder="nbrChef"
+            placeholderTextColor="#ffffff"
+            style={styles.input}
+            value={nbrChef}
+            // editable={!isLoading}
+            onChangeText={onChangenbrChefHandler}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <TextInput
+            placeholder="nbrCleaningWorker"
+            placeholderTextColor="#ffffff"
+            style={styles.input}
+            value={nbrCleaningWorker}
+            // editable={!isLoading}
+            onChangeText={onChangenbrCleaningWorkerHandler}
+          />
+        </View>
+
+        <View>
           <Button
             title="Submit"
             onPress={onSubmitFormHandler}
             style={styles.submitButton}
-            disabled={isLoading}
+            // disabled={isLoading}
           />
-        </View> */}
+        </View>
       </View>
     </ScrollView>
   );
@@ -138,7 +204,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#252526",
     alignItems: "center",
     justifyContent: "center",
-    // marginTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+    // marginTop: Platform.OS === "android" ? 0 : Constants.statusBarHeight,
   },
   formHeading: {
     color: "#ffffff",
