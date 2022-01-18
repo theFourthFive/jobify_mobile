7862acd6f54cb3f150d0e0765f086b05062ebafb
AhmedBouafif <ahmedbouafif500@gmail.com>
2022-01-18 12:03:59 +0100

    retouches for design

--- jobify/components/UserHire.js
+++ jobify/components/UserHire.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import server from "./ipConfig/serverIp.js";
 import axios from "axios";
 import moment from "moment";
-
+const b100 = "100%"
 const UserHire=(props)=> { 
     console.log("===============>",props)
     var [offers , setoffers] = useState([])
@@ -19,8 +19,8 @@ const UserHire=(props)=> {
       })
     },[])
     return (
-      <ScrollView style={styles.users} >
-        <View  >
+      <ScrollView>
+      <View style={styles.users}  >
             <View style={styles.user}  >
                 <Image  style={styles.img} source={{ uri: props.route.params.imageUrl }}></Image>
                 <View style={styles.userr}>
@@ -30,64 +30,73 @@ const UserHire=(props)=> {
                 <AirbnbRating style={styles.star} count={props.route.params.avgRating} isDisabled={true} onFinishRating={(x)=>{alert("thanks for your help" , x)}} reviews = {['Terrible', 'Bad', 'Okay', 'Good', 'Great']} defaultRating={9} size={25} showRating={true} startingValue={5}  ratingColor='#f94368' ratingBackgroundColor='#f9b313' type='custom' />
                     
             </View>
-        </View>
+        
         <View style={styles.center}>
         <Text style={styles.text}>
           Chose The Event That You Want This Woker To Work On In Just One Click On It
         </Text>
-        <View>
+        </View>
+        </View>
+      <ScrollView >
+        <View style={styles.wait}>
           {offers.filter(e=>e.companyCompanyId===38).map((e,i)=>{
             return (
-              <ScrollView key={i}>
-                <View style={styles.off}>
+                <View key={i} style={styles.off}>
                   <Image style={styles.image} source={{ uri: e.imageUri }}></Image>
                   <Text style={styles.text}>Type : {e.eventName}</Text>
                   <Text style={styles.text}>Place : {e.location}</Text>
                   <Text style={styles.text}>Posted At : {moment(e.createdAt).fromNow()}</Text>
                 </View>
-              </ScrollView>
             )
           })}
         </View>
-        </View>
       </ScrollView>
+      </ScrollView>
+        
+     
     );
 }
 
 const styles = StyleSheet.create({
     users:{
         flex:1,
-        width:350,
+        width:b100,
         alignContent:'center',
         alignSelf:'center',
         
       },
       text:{
         alignSelf:'center',
+        alignItems:'center',
+        alignContent:'center',
+        
       },
       center:{
         alignItems:'center',
         alignSelf:'center',
         
       },
+      wait:{
+        alignSelf:'center',
+        alignItems:'center',
+        alignContent:'center',
+      },
       off:{
         flex:1,
         margin:5,
-        width: 209,
+        width: b100,
         height: 234,
         borderRadius:15,
+      
       },
       image:{
         flex:1,
-        width: 195,
+        width: 290,
         borderRadius:15,
       },
       text:{
         color:'#00BFFF',
         
-      },
-      text:{
-        color:'#00BFFF',
       },
       img:{
           width:70,
