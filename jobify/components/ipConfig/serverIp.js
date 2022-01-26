e190844f70fc526d3bae7cecb488fb066f684451
AhmedAzizGHATTAS <azizghattas552@gmail.com>
2022-01-26 09:13:38 +0100

    pulling day of the techint

--- jobify/components/ipConfig/serverIp.js
+++ jobify/components/ipConfig/serverIp.js
@@ -1,4 +1,4 @@
 // var server = { Ip: "http://192.168.11.190:3000" };
-var server = { Ip: "http://192.168.11.182:3200" };
+var server = { Ip: "http://192.168.11.50:3200" };
 
 export default server;
